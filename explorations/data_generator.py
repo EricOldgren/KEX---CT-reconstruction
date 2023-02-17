c50a40e9d47a23ad6791a22d17664e37d1eb6cc8
Knut <salomonsson.knut@gmail.com>
2023-02-17 10:27:02 +0100

    unstructured phantoms

--- explorations/data_generator.py
+++ explorations/data_generator.py
@@ -5,17 +5,8 @@ from odl.phantom import ellipsoid_phantom, shepp_logan_ellipsoids
 from odl import DiscretizedSpace, DiscretizedSpaceElement
 import matplotlib.pyplot as plt
 
-#  #           value  axisx  axisy     x       y  rotation
-#     return [[2.00, .6900, .9200, 0.0000, 0.0000, 0],
-#             [-.98, .6624, .8740, 0.0000, -.0184, 0],
-#             [-.02, .1100, .3100, 0.2200, 0.0000, -rad18],
-#             [-.02, .1600, .4100, -.2200, 0.0000, rad18],
-#             [0.01, .2100, .2500, 0.0000, 0.3500, 0],
-#             [0.01, .0460, .0460, 0.0000, 0.1000, 0],
-#             [0.01, .0460, .0460, 0.0000, -.1000, 0],
-#             [0.01, .0460, .0230, -.0800, -.6050, 0],
-#             [0.01, .0230, .0230, 0.0000, -.6060, 0],
-#             [0.01, .0230, .0460, 0.0600, -.6050, 0]]
+def ellipsoid_structure(value: float, x_radius: float, y_radius: float, x_center: float, y_center: float, rotation: float):
+    return [value, x_radius, y_radius, x_center, y_center, rotation]
 
 def random_ellipsoid(min_pt, max_pt, value = 0.5):
     "Generate an ellips in the rectangular region bordered by mint_pt, max_pt"
@@ -30,6 +21,18 @@ def random_ellipsoid(min_pt, max_pt, value = 0.5):
     #        value  axisx  axisy   x    y   rotation(rad)
     return [value, rx,     ry,centerx, centery, np.random.random()*np.pi]
 
+def unstructured_random_phantom(reco_space: DiscretizedSpace, num_ellipses = 10):
+    "Phantom of random ellipses. More random and doesn't look like a brain. Maybe useful for more diverse data."
+
+    ellipsoids = []
+    for _ in range(num_ellipses):
+        ellipsoids.append(random_ellipsoid([-1.0, -1.0], [1.0, 1.0], value = np.random.uniform(0.1, 0.6)))
+
+    res = ellipsoid_phantom(reco_space, ellipsoids)
+    res /= np.max(res)
+
+    return res
+
 def random_phantom(reco_space: DiscretizedSpace, num_ellipses = 10)->DiscretizedSpaceElement:
 
     brain_center = np.random.uniform(-0.1, 0.1, 2)
@@ -37,12 +40,9 @@ def random_phantom(reco_space: DiscretizedSpace, num_ellipses = 10)->Discretized
     min_s = np.min(brain_r) / np.sqrt(2)
     skull = [1.0, brain_r[0], brain_r[1], brain_center[0], brain_center[1], 0.0]
     brain = [-0.5, brain_r[0]*0.95, brain_r[1]*0.95, brain_center[0], brain_center[1], 0.0]
- 
-    print("Skull: ", skull)
-    print("Brain: ", brain)
 
     ellipses = [skull, brain]
-    for i in range(num_ellipses):
+    for _ in range(num_ellipses):
         ellipses.append(random_ellipsoid( brain_center-min_s, brain_center+min_s, value = np.random.uniform(0.1, 0.3)))
     
     res = ellipsoid_phantom(reco_space, ellipsoids=ellipses)
@@ -57,5 +57,9 @@ if __name__ == '__main__':
         phantom = random_phantom(reco_space)
         print(phantom.shape)
         plt.imshow(phantom.asarray())
+        plt.show()
+
+        unstructured = unstructured_random_phantom(reco_space)
+        plt.imshow(unstructured)
         plt.show()
         
