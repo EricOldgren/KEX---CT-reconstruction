2a0965f433b845242b2e7e8489765addcd9cea48
Knut <salomonsson.knut@gmail.com>
2023-03-16 21:50:51 +0100

    ramlak

--- src/explorations/ramlak.py
+++ src/explorations/ramlak.py
@@ -5,7 +5,7 @@ from utils.geometry import Geometry, setup, BasicModel
 import random
 from models.analyticmodels import RamLak
 
-ANGLE_RATIOS = [1.0, .8]
+ANGLE_RATIOS = [0.5]
 EPOPCHS =      [100, 60]
 TRAINED = {}
 
@@ -13,8 +13,8 @@ TRAINED = {}
 for ar in ANGLE_RATIOS:
 
     #For maximazing omega - phi_size ~ pi * t_size / 2
-    geometry = Geometry(ar, phi_size=300, t_size=150)
-    (test_sinos, test_y, _, _), geometry = setup(geometry, train_ratio=1.0, num_samples=10)
+    geometry = Geometry(ar, phi_size=450, t_size=300)
+    (test_sinos, test_y, _, _) = setup(geometry, train_ratio=1.0, num_samples=10)
     analytic = RamLak(geometry)
 
     analytic.visualize_output(test_sinos, test_y, lambda diff : torch.mean(diff*diff))
