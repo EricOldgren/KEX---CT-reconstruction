8b7ebe04d7e41896a2cf78ce7c559b0ceffaabfb
Knut <salomonsson.knut@gmail.com>
2023-03-29 16:14:09 +0200

    saving geometry in fbpnet

--- src/explorations/full_angle.py
+++ src/explorations/full_angle.py
@@ -6,13 +6,13 @@ from models.fbpnet import FBPNet
 import random
 
 ANGLE_RATIOS = [0.5]#[0.8, 0.85, 0.9, 0.95, 1.0]
-EPOPCHS =      [100]#[100, 100, 100,  100, 60]
+EPOPCHS =      [3]#[100, 100, 100,  100, 60]
 TRAINED = {}
 LAMBDA  = 0.01 #regularization parameter
 
 for ar, n_epochs in zip(ANGLE_RATIOS, EPOPCHS):
     geometry = Geometry(ar, 300, 150) #50,40
-    (train_sinos, train_y, test_sinos, test_y) = setup(geometry, num_samples=10,use_realistic=True,data_path="data/kits_phantoms_256.pt")
+    (train_sinos, train_y, test_sinos, test_y) = setup(geometry, num_samples=3,use_realistic=False,data_path="data/kits_phantoms_256.pt")
     model = FBPNet(geometry, n_fbps=5)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
@@ -22,9 +22,8 @@ for ar, n_epochs in zip(ANGLE_RATIOS, EPOPCHS):
 
     for epoch in range(n_epochs):
         if epoch % 10 == 0:
-            model.visualize_output(test_sinos, test_y, loss_fn, output_location="show")
+            model.visualize_output(test_sinos, test_y, loss_fn, output_location="file")
         for sinos, y in dataloader:
-            print(sinos.size())
             out = model(sinos)
 
             loss = loss_fn(out - y) #+ abs(sum(out[int(geometry.omega):]))
@@ -35,4 +34,6 @@ for ar, n_epochs in zip(ANGLE_RATIOS, EPOPCHS):
         print(f"epoch {epoch} loss: {loss.item()}")
     
     TRAINED[ar] = model
+
+torch.save(TRAINED[0.5].state_dict(), "test-path.pt")
 
