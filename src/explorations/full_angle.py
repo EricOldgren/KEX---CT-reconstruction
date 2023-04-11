ad144e2a4e1d20179cef8a86e03704fc92acd945
Knut <salomonsson.knut@gmail.com>
2023-04-11 09:43:04 +0200

    merge

--- src/explorations/full_angle.py
+++ src/explorations/full_angle.py
@@ -13,7 +13,7 @@ LAMBDA  = 0.01 #regularization parameter
 for ar, n_epochs in zip(ANGLE_RATIOS, EPOPCHS):
     geometry = Geometry(ar, 300, 150) #50,40
 
-    (train_sinos, train_y, test_sinos, test_y) = setup(geometry, num_samples=3400,use_realistic=True,data_path="data/kits_phantoms_256.pt")
+    (train_sinos, train_y, test_sinos, test_y) = setup(geometry, num_to_generate=3400,use_realistic=True,data_path="data/kits_phantoms_256.pt")
     model = FBPNet(geometry, n_fbps=3)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
