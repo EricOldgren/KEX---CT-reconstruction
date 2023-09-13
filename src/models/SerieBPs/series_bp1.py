d8e5137b39f6a7e3f2b4a5cf6b95fd361f8f8dd9
Emanuel Strom <emastr@kth.se>
2023-09-13 10:01:59 +0200

    training series_bpv1

--- src/models/SerieBPs/series_bp1.py
+++ src/models/SerieBPs/series_bp1.py
@@ -10,15 +10,16 @@ from models.modelbase import FBPModelBase, load_model_checkpoint, PathType
 
 class Series_BP(FBPModelBase):
 
-    def __init__(self, geometry: FBPGeometryBase, ar: float, M: int, K: int, polynomial_family_key: int = Legendre.key):
+    def __init__(self, geometry: FBPGeometryBase, ar: float, M: int, K: int, polynomial_family_key: int = Legendre.key, strict_moments = True):
         assert 0 < ar <= 1.0, f"angle ratio, {ar} is invalid"
         super().__init__()
         self.geometry = geometry
-        self._init_args = (ar, M, K, polynomial_family_key)
+        self._init_args = (ar, M, K, polynomial_family_key, strict_moments)
 
         n_known_angles = geometry.n_known_projections(ar)
         self.M, self.K = M, K
         self.PolynomialFamily = POLYNOMIAL_FAMILY_MAP[polynomial_family_key]
+        self.strict_moments = strict_moments
 
         h, w, c = n_known_angles, geometry.projection_size, 1
         next_c = lambda c : 8 if c == 1 else min(64, c*2)
@@ -31,7 +32,7 @@ class Series_BP(FBPModelBase):
             w = (w-4)//2 + 1
  
         self.moment_mask = get_moment_mask(torch.zeros((1,M,K), device=DEVICE))
-        n_coeffs = self.moment_mask.count_nonzero()
+        n_coeffs = M*K  #self.moment_mask.count_nonzero()
         self.conv_layers = nn.ModuleList(conv_layers)
         self.lin_out = nn.Linear(64, n_coeffs, dtype=CDTYPE, device=DEVICE)
     
@@ -47,9 +48,9 @@ class Series_BP(FBPModelBase):
 
         out = torch.mean(out, dim=(-1,-2), dtype=CDTYPE)
 
-        out = self.lin_out(out)
-        coefficients = torch.zeros((N, self.M, self.K), device=DEVICE, dtype=CDTYPE)
-        coefficients[:, self.moment_mask] += out
+        coefficients = self.lin_out(out).reshape(N, self.M, self.K)
+        if self.strict_moments:
+            coefficients[:, ~self.moment_mask] *= 0
 
         return self.geometry.synthesise_series(coefficients, self.PolynomialFamily)
     
@@ -70,18 +71,20 @@ if __name__ == "__main__":
     from torch.utils.data import TensorDataset, DataLoader
     from utils.tools import MSE, htc_score
     from utils.polynomials import Legendre, Chebyshev
-    from utils.data import get_htc2022_train_phantoms, get_htc_trainval_phantoms
+    from utils.data import get_htc2022_train_phantoms, get_htc_trainval_phantoms, GIT_ROOT
     from geometries import HTC2022_GEOMETRY
-    from models.modelbase import plot_model_progress
+    from models.modelbase import plot_model_progress, save_model_checkpoint
     import matplotlib
     import matplotlib.pyplot as plt
     from statistics import mean
     ar = 0.25
     geometry = HTC2022_GEOMETRY
     PHANTOMS, VALIDATION_PHANTOMS = get_htc_trainval_phantoms()
+    print("phantoms are loaded")
     SINOS = geometry.project_forward(PHANTOMS)
+    print("sinos are calculated")
     dataset = TensorDataset(PHANTOMS, SINOS)
-    dataloader = DataLoader(dataset, batch_size=8, shuffle=True)
+    dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
 
     M, K = 120, 60
 
@@ -89,7 +92,7 @@ if __name__ == "__main__":
     print(model)
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
-    n_epochs = 200
+    n_epochs = 300
     for epoch in range(n_epochs):
         sino_losses, recon_losses = [], []
         for phantom_batch, sino_batch in dataloader:
@@ -110,6 +113,7 @@ if __name__ == "__main__":
     _, known_angles = geometry.zero_cropp_sinos(VALIDATION_SINOS, ar, 0)
 
     disp_ind = 1
+    save_model_checkpoint(model, optimizer, mse_sinos, ar, GIT_ROOT / f"data/models/serries_bpv1.1_sino_mse_{mse_sinos.item()}.pt")
     plot_model_progress(model, VALIDATION_SINOS, known_angles, VALIDATION_PHANTOMS, disp_ind=disp_ind)
     plt.show()
     
