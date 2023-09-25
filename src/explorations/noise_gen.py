d09ff59b14177aeba757820d17cc30a8df1454b7
Knut <salomonsson.knut@gmail.com>
2023-09-26 01:41:08 +0200

    segmentation and scale

---
+++ src/explorations/noise_gen.py
@@ -0,0 +1,42 @@
+import torch
+
+from utils.tools import DEVICE, DTYPE, CDTYPE, MSE, GIT_ROOT
+from utils.data import disc_phantom, get_htc2022_train_phantoms
+from geometries import HTC2022_GEOMETRY, htc_sino_var
+import matplotlib.pyplot as plt
+
+# phantoms = get_htc2022_train_phantoms()
+sinos = torch.stack(torch.load(GIT_ROOT/"data/HTC2022/HTCTrainingData.pt"))[:, :720]
+phantoms = HTC2022_GEOMETRY.fbp_reconstruct(sinos)
+# sinos = HTC2022_GEOMETRY.project_forward(phantoms)
+
+noisy = sinos + torch.randn(sinos.shape, device=DEVICE, dtype=DTYPE)*htc_sino_var
+
+recons = HTC2022_GEOMETRY.fbp_reconstruct(noisy)
+
+print(MSE(noisy, sinos))
+print(MSE(phantoms, recons))
+
+disp_ind = 2
+plt.subplot(121)
+plt.imshow(noisy[disp_ind].cpu())
+plt.colorbar()
+plt.title("noisy")
+plt.subplot(122)
+plt.imshow(sinos[disp_ind].cpu())
+plt.colorbar()
+plt.title("gt")
+
+plt.figure()
+plt.subplot(121)
+plt.imshow(recons[disp_ind].cpu())
+plt.colorbar()
+plt.title("recon")
+plt.subplot(122)
+plt.imshow(phantoms[disp_ind].cpu())
+plt.title("gt")
+
+plt.show()
+
+
+
--- src/geometries/__init__.py
+++ src/geometries/__init__.py
@@ -7,5 +7,7 @@ AVAILABLE_FBP_GEOMETRIES: List[FBPGeometryBase]  = [ParallelGeometry, FlatFanBea
 
 
 scale_factor = 1.0
-threshhold_value = 0.02
+htc_th = 0.02
+htc_sino_var = 2e-2
+htc_mean_attenuation = 0.03
 HTC2022_GEOMETRY = FlatFanBeamGeometry(720, 560, 410.66*scale_factor, 543.74*scale_factor, 112.0*scale_factor, [-38*scale_factor,38*scale_factor, -38*scale_factor,38*scale_factor], [512, 512])
