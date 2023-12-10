78050a92548d80caf3084ed995854db7e687a586
Knut <salomonsson.knut@gmail.com>
2023-12-10 10:10:10 +0100

    .

--- src/models/FNOBPs/fnobp2d.py
+++ src/models/FNOBPs/fnobp2d.py
@@ -10,14 +10,15 @@ from typing import Any, List
 
 class FNO_BP2D(FBPModelBase):
 
-    def __init__(self, geometry: FBPGeometryBase, ar: float, hidden_layers: List[int], modes_y = 50, modes_x=50, M = 100, K = 100, PolynomialFamilyKey: int = Chebyshev.key, l2_reg = 0.01):
+    def __init__(self, geometry: FBPGeometryBase, ar: float, hidden_layers: List[int], modes_y = 50, modes_x=50, M = 100, K = 100, PolynomialFamilyKey: int = Chebyshev.key, l2_reg = 0.01, use_base_filter=False):
         super().__init__()
-        self._init_args = (ar, hidden_layers, modes_y, modes_x, M, K, PolynomialFamilyKey, l2_reg)
+        self._init_args = (ar, hidden_layers, modes_y, modes_x, M, K, PolynomialFamilyKey, l2_reg, use_base_filter)
         self.geometry = geometry
         self.ar = ar
         self.M, self.K = M, K
         self.PolynomialFamily = POLYNOMIAL_FAMILY_MAP[PolynomialFamilyKey]
         self.l2_reg = torch.nn.Parameter(torch.tensor(l2_reg), requires_grad=False)
+        self.use_base_filter = use_base_filter
 
         if modes is None:
             modes = self.geometry.projection_size // 2
@@ -40,7 +41,9 @@ class FNO_BP2D(FBPModelBase):
     
     def get_extrapolated_filtered_sinos(self, sinos: torch.Tensor, known_angles: torch.Tensor, angles_out = None):
         sinos = self.get_extrapolated_sinos(sinos, known_angles)
-        return self.fno2d(sinos[:, None])[:, 0]
+        if not self.use_base_filter:
+            return self.fno2d(sinos[:, None])[:, 0]
+        return self.fno2d(sinos[:, None])[:, 0] + self.geometry.inverse_fourier_transform(self.geometry.ram_lak_filter()*self.geometry.fourier_transform(sinos))
 
     def forward(self, sinos: torch.Tensor, known_angles: torch.Tensor, angles_out = None, use_relu = True):
         if use_relu:
